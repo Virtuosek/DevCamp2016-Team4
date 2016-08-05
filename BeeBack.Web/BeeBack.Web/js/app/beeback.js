b9987357026241029362da664bc23ad4808c3f42
Renaud Dumont <renaud.dumont@live.fr>
2016-08-05 22:06:23 +0200

    Added subscription button + toast for notifications

---
+++ BeeBack.Web/BeeBack.Web/js/app/beeback.js
@@ -0,0 +1,29 @@
+﻿BEEBACK = {
+    // Default (no) area
+    default: {
+        // Logic common to the whole area
+        common: {
+            init: function () {
+                toastr.options.closeButton = true;
+
+                $(".subscribe").click(function () {
+                    if ($(this).hasClass("subscribe-add")) {
+                        var activityId = $(this).attr("data-activityId");
+                        BEEBACK_API.subscriptions.add(activityId, function () {
+                            toastr.success("Vous êtes abonné.");
+                        }, function () {
+                            toastr.warning("Nous n'avons pas pu vous abonner. Life is unfair, get used to it.")
+                        });
+                    } else if ($(this).hasClass("subscribe-remove")) {
+                        var activityId = $(this).attr("data-activityId");
+                        BEEBACK_API.subscriptions.remove(activityId, function () {
+                            toastr.success("Vous êtes abonné.");
+                        }, function () {
+                            toastr.warning("Nous n'avons pas pu vous abonner. Life is unfair, get used to it.")
+                        });
+                    }
+                });
+            }
+        }
+    }
+};
