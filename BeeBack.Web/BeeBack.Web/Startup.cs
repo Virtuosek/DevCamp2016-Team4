42ee8bf17af46b132954f9304a9298c13a543c0c
MatthieuVdh <mv@live.be>
2016-08-05 11:32:19 +0200

    Add web solution

---
+++ BeeBack.Web/BeeBack.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeeBack.Web.Startup))]
+namespace BeeBack.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
