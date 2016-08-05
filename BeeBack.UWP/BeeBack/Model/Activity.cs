db83703632fc5acc5776cb1be51f055e9e92febe
adsli <adsli@DESKTOP-QT8IS1T>
2016-08-05 11:46:14 +0200

    First model

---
+++ BeeBack.UWP/BeeBack/Model/Activity.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBack.Model
+{
+    public class Activity : ObservableObject
+    {
+        public int ID { get; set; }
+        public int FkUser { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BeeBack.UWP/BeeBack/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeBack.Model
+{
+    public class User : ObservableObject
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EMailAddress { get; set; }
+        public string Password { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
