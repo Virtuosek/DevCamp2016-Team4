fac125822dcc25409fe802744165203086b4daa0
MatthieuVdh <mv@live.be>
2016-08-06 12:00:19 +0200

    Fix

--- BeeBack.UWP/BeeBack/Design/DesignDataService.cs
+++ BeeBack.UWP/BeeBack/Design/DesignDataService.cs
@@ -42,29 +42,29 @@ namespace BeeBack.Design
             return Task.Run(() =>
             {
                 List<Activity> activities = new List<Activity>();
-                Activity a;
-                User u;
-                for (int i = 0; i < 50; i++)
-                {
-                    a = new Activity();
-                    a.Description = " ksjf mlks jflksfs kjlkds jfmlks jflkds jflkds jflkds jflksd jfl kjld mj dmlkq jmlk jflkjf dsml lkj fmlksj mlkds lksj flkds jflkjs lkfj dsjf lk";
-                    a.Title = $"{i} Conduire les enfants à l'école tous les mardis matin";
-                    for (int j = 0; j < 10; j++)
-                    {
-                        u = new User();
-                        u.EMailAddress = "tot@totofds.be";
-                        u.Name = $"{j}Smith";
-                        u.FirstName = "John";
-                        u.MobilePhone = "+32475123456";
-                        if (j == 0)
-                        {
-                            a.Owner = u;
-                        }
-                        u.Activities.Add(a);
-                        a.Members.Add(u);
-                    }
-                    activities.Add(a);
-                }
+                //    Activity a;
+                //    User u;
+                //    for (int i = 0; i < 50; i++)
+                //    {
+                //        a = new Activity();
+                //        a.Description = " ksjf mlks jflksfs kjlkds jfmlks jflkds jflkds jflkds jflksd jfl kjld mj dmlkq jmlk jflkjf dsml lkj fmlksj mlkds lksj flkds jflkjs lkfj dsjf lk";
+                //        a.Title = $"{i} Conduire les enfants à l'école tous les mardis matin";
+                //        for (int j = 0; j < 10; j++)
+                //        {
+                //            u = new User();
+                //            u.EMailAddress = "tot@totofds.be";
+                //            u.Name = $"{j}Smith";
+                //            u.FirstName = "John";
+                //            u.MobilePhone = "+32475123456";
+                //            if (j == 0)
+                //            {
+                //                a.Owner = u;
+                //            }
+                //            u.Activities.Add(a);
+                //            a.Members.Add(u);
+                //        }
+                //        activities.Add(a);
+                //    }
 
                 return activities;
             });
@@ -81,7 +81,7 @@ namespace BeeBack.Design
             throw new NotImplementedException();
         }
 
-        public Task<User> GetUser(Guid UserID)
+        public Task<User> GetUser(string UserID)
         {
             throw new NotImplementedException();
         }
